63e5003afb6019c3cfa0e975a8880f710f728832
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-11-07 19:51:13 -0800

    AnimatedLetters imported into Home index.js, useState module used

--- min-react-portfolio/src/components/Home/index.js
+++ min-react-portfolio/src/components/Home/index.js
@@ -1,11 +1,14 @@
 import LogoPic from '../../assets/images/Sule.png';
 import './index.scss';
 import { Link } from 'react-router-dom';
+import AnimatedLetters from '../AnimatedLetters';
+import { useState } from 'react';
 
 
 const Home = () => {
-    // const [ letterClass, setLetterClass] = useState('text-animate')
-    // const nameArray = []
+    const [ letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['🌅','မ','င်္ဂ','လ','ာ','ပ','ါ','🧋',]
+    const welcomeArray = ['😎','W','e','l','c','o','m','e','_','t','o','_','m','y','^','🏘️','^','o','n','_','t','h','e','🌐',]
 
     return(
         <main>
@@ -15,13 +18,15 @@ const Home = () => {
                         🌅 မင်္ဂလာပါ 🧋
                     </h1>
                         <h2>
-                        😎 Welcome to my 🏘️ on the 🌐
+                        <AnimatedLetters letterClass={letterClass}
+                        strArray={welcomeArray}
+                        idx={22}/>
                         </h2>
                             <h3>
                                 <span> const Min = </span> Full-Stack Web Dev 👾 MERN | Public Policy Research 🔬 | Burmese Foods Entrepreneurship 🍛 | Street Photography 📸 | USPTA Tennis Instructor 🎾 | PADI Scuba 🤿 | UNESCO Volunteer 🏛️
                             </h3>
                                 <img className='coverP' 
-                                src={LogoPic} alt="My copyrighted Sule Protest: February, Yangon 2021">
+                                src={LogoPic} alt="©copyrighted Sule Protest: February, Yangon 2021">
                                 </img>
                     <Link to="/contact" className='flat-button'>📧 CONTACT ME 🤙 </Link>
                 </div>
