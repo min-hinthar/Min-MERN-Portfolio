8d94caac18fc0bdd32e8992020316107fc12b46f
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-11-15 21:43:10 -0800

    portfolio folder created under public root to store cover images of projects, data folder created to store portfolio.json data to renderPortfoloio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,28 +1,58 @@
 import { React, useEffect, useState } from 'react';
-import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
+import portfolioData from '../../data/portfolio.json';
 
 const Portfolio = () => {
-    const [ letterClass, setLetterClass] = useState('text-animate')
+    const [ letterClass, setLetterClass] = useState('text-animate');
     
     useEffect(() => {
-        setTimeout(() => {
+        const timer = setTimeout(() => {
             return setLetterClass('text-animate-hover')
-        }, 3000)
-    }, [])
+        }, 3000);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    });
+    
+    const renderPortfolio = (portfolio) => {
+        return (
+            <div className='images-container'>
+                {
+                    portfolio.map((port, idx) => {
+                        return (
+                            <div className='image-box' key={idx}>
+                                <img 
+                                    src={port.cover}
+                                    className="portfolio-image" 
+                                    alt="cover image of project" 
+                                />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        );
+    }
 
     return (
-        <div className="container portfolio-page">
-            <div className='text-zone'>
-                <h1 className="page-title">
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={"PORTFOLIO".split("")}
-                        idx={15}
-                    />
-                </h1>
+        <>
+            <div className="container portfolio-page">
+                <div className='text-zone'>
+                    <h1 className="page-title">
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={"PORTFOLIO".split("")}
+                            idx={15}
+                        />
+                    </h1>
+                    <div>
+                        {renderPortfolio(portfolioData.portfolio)}
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
