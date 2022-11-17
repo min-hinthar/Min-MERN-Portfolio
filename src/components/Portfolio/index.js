c326c843f473580dfad7eeaa1cb0859018cc05dc
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-11-16 16:45:43 -0800

    portfolio-image content layer css styling updated, hover effect, text-zone viewwidth 80vw

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -28,6 +28,17 @@ const Portfolio = () => {
                                     className="portfolio-image" 
                                     alt="cover image of project" 
                                 />
+                                <div className='content'>
+                                    <p className='title'>
+                                        {`${port.title}`}
+                                    </p>
+                                    <h4 className='description'>
+                                        {`${port.description}`}
+                                    </h4>
+                                    <button className='btn' onClick={()=> window.open(`${port.url}`)}>
+                                        View
+                                    </button>
+                                </div>
                             </div>
                         )
                     })
